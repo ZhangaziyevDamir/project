3b6725eefd8d5e57b3ed93646687dbf087679da9
Damir <example@gmail.com>
2024-07-10 19:40:36 +0300

    logycal operation

--- script.js
+++ script.js
@@ -8,10 +8,23 @@
 //     console.log("The number is equal to 100");
 // } 
 // (num == 100) ? console.log("Verno") : console.log('Neverno')
-num = 51;
-switch (num){
-    case 49: console.log('Neverno'); break;
-    case 50: console.log('Verno'); break;
-    case 100: console.log('Neverno'); break;
-    default: console.log('Poprobuite zanovo'); break;
+// num = 51;
+// switch (num){
+//     case 49: console.log('Neverno'); break;
+//     case 50: console.log('Verno'); break;
+//     case 100: console.log('Neverno'); break;
+//     default: console.log('Poprobuite zanovo'); break;
+// }
+
+// console.log( 5 === 5 && 3 > 1 || 5);
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
 }
+
+
