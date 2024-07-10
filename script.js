eaf27a0b36e4bd83ac7885ac6738997fdb9ae91b
Damir <example@gmail.com>
2024-07-10 19:56:09 +0300

    cycle

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
-// "use strict";
-// const num = 100;
-// if (num > 120){
-//     console.log("Error over 120");
-// }else if (num < 120) {
-//     console.log("Error under 120");
-// } else if (num == 100){
-//     console.log("The number is equal to 100");
-// } 
-// (num == 100) ? console.log("Verno") : console.log('Neverno')
-// num = 51;
-// switch (num){
-//     case 49: console.log('Neverno'); break;
-//     case 50: console.log('Verno'); break;
-//     case 100: console.log('Neverno'); break;
-//     default: console.log('Poprobuite zanovo'); break;
+// "use strict"
+// let num = 50
+// while (num < 55){
+//     console.log(num);
+//     num++;
+// };
+// let num = 50;
+// do {
+//     console.log(num);
+//     num++;
 // }
-
-// console.log( 5 === 5 && 3 > 1 || 5);
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
-
-
-if (hamburger && cola || fries === 3 && nuggets) {
-   console.log('Done!')
-}
-
-
+// while(num < 55);
+let num = 50;
+for (let i = 1; i <= 10; i++){
+    if( i === 5){
+        continue;
+    }
+    console.log(i);
+    
+};
